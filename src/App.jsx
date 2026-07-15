8ba46285ec20de0d5595ed6fe07b4e18ce2d23d1
Ad-rian27 <rian270627@gmail.com>
2026-07-16 01:56:43 +0530

    Added ViewMyPost option

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import SignIn from './components/SignIn'
 import CreatePost from './components/CreatePost'
 import ViewAll from './components/ViewAll'
+import ViewMyPost from './components/ViewMyPost'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -20,6 +21,7 @@ function App() {
   <Route path='/signin' element={<SignIn />} />
   <Route path='/create' element={<CreatePost />} />
   <Route path='/viewall' element={<ViewAll />} />
+  <Route path='/viewmyposts' element={<ViewMyPost />} />
 </Routes>
 </BrowserRouter>
     </>
--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,10 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from 'react';
+import { Link, useNavigate } from 'react-router-dom';
 
 const NavigationBar = () => {
+
+    const navigate = useNavigate();
+
+    const LogOut = () => {
+        sessionStorage.clear();
+        navigate("/");
+    };
+
     return (
         <div>
-
             <nav
                 className="navbar navbar-expand-lg navbar-dark shadow-sm py-3"
                 style={{ backgroundColor: "#8B5E3C" }}
@@ -12,7 +19,7 @@ const NavigationBar = () => {
                 <div className="container">
 
                     <Link className="navbar-brand fw-bold fs-4" to="/">
-                        Pet Boarding Management
+                        Blog App
                     </Link>
 
                     <button
@@ -32,7 +39,7 @@ const NavigationBar = () => {
                                     className="btn btn-outline-light rounded-pill px-4 fw-semibold"
                                     to="/"
                                 >
-                                    Add Pet
+                                    SignUp
                                 </Link>
                             </li>
 
@@ -42,26 +49,47 @@ const NavigationBar = () => {
                                     style={{ color: "#8B5E3C" }}
                                     to="/signin"
                                 >
-                                    View Pets
+                                    SignIn
                                 </Link>
                             </li>
+
                             <li className="nav-item mx-2 mt-2 mt-lg-0">
                                 <Link
                                     className="btn btn-light rounded-pill px-4 fw-semibold"
                                     style={{ color: "#8B5E3C" }}
                                     to="/create"
                                 >
-                                    View Pets
+                                    Create Post
                                 </Link>
                             </li>
+
                             <li className="nav-item mx-2 mt-2 mt-lg-0">
                                 <Link
                                     className="btn btn-light rounded-pill px-4 fw-semibold"
                                     style={{ color: "#8B5E3C" }}
                                     to="/viewall"
                                 >
-                                    View Pets
+                                    View All
                                 </Link>
+                            </li>
+
+                            <li className="nav-item mx-2 mt-2 mt-lg-0">
+                                <Link
+                                    className="btn btn-light rounded-pill px-4 fw-semibold"
+                                    style={{ color: "#8B5E3C" }}
+                                    to="/viewmyposts"
+                                >
+                                    View My Posts
+                                </Link>
+                            </li>
+
+                            <li className="nav-item mx-2 mt-2 mt-lg-0">
+                                <button
+                                    onClick={LogOut}
+                                    className="btn btn-success rounded-pill px-4 fw-semibold"
+                                >
+                                    Logout
+                                </button>
                             </li>
 
                         </ul>
@@ -69,9 +97,8 @@ const NavigationBar = () => {
 
                 </div>
             </nav>
-
         </div>
-    )
-}
+    );
+};
 
-export default NavigationBar
+export default NavigationBar;
