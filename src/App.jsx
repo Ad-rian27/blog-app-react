ac31c990c66d1d21c2e250a462d6665f0d74985b
Ad-rian27 <rian270627@gmail.com>
2026-07-16 01:23:23 +0530

    Added view all option

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import SignUp from './components/SignUp'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import SignIn from './components/SignIn'
 import CreatePost from './components/CreatePost'
+import ViewAll from './components/ViewAll'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -18,6 +19,7 @@ function App() {
   <Route path='/' element={<SignUp />} />
   <Route path='/signin' element={<SignIn />} />
   <Route path='/create' element={<CreatePost />} />
+  <Route path='/viewall' element={<ViewAll />} />
 </Routes>
 </BrowserRouter>
     </>
